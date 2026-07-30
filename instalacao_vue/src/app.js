42ee670bab2da941ff6cad43f5f03a8ff931a72f
ominivac <robertokbs@gmail.com>
2026-07-29 21:57:05 -0300

    aula 03 input e data binding

--- instalacao_vue/src/app.js
+++ instalacao_vue/src/app.js
@@ -1,10 +1,19 @@
 const MyNameApp  = {
     data() {
         return {    
-            name: 'Rafael',
+            name: '',
             age: 22,
-            job: 'Desenvolvedor'
+            job: 'Desenvolvedor',
+            input_name: ''
         }
+    },
+    methods: {
+        submitForm(e){
+            e.preventDefault();
+            console.log(this.input_name);
+            this.name = this.input_name;
+    }
+        
     }
 }   
 
